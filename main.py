3427f4b6ea94f0bb436eccf6fad7127e354697c5
Roozbeh-web <roozbeh279@gmail.com>
2020-11-30 11:24:43 -0500

    add comments

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+#import requests module for getting the json file
 import requests
+#import json module for loading the json file
 import json
+#import urllib module for getting the image
 import urllib.request
+#import datetime module for getting the today's date
 from datetime import datetime
 
+
+
+#get and laod the json file and store it in a variable
 jsonData = json.loads(requests.get(f"https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US").text)
 
+#store the image url in a variable
 imageUrl = "https://www.bing.com" + jsonData["images"][0]["url"]
 
+#store today's date in a variable
 today = datetime.today().strftime("%Y-%m-%d")
 
+#store the full path in a variable
 fullPath = "/home/wabbajack/Pictures/" + today + ".jpg"
 
+#use urllib module to get the image and save it in defined path
 urllib.request.urlretrieve(imageUrl,fullPath)
